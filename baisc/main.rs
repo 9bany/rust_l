08101cf8c1b0935e2025d4a88324d5e8f4bc9c2d
9bany <bany.y0599@gmail.com>
2023-10-11 01:17:01 +0000

    ft: func in rust

--- baisc/main.rs
+++ baisc/main.rs
@@ -1,3 +1,20 @@
+
+// normal func
+fn sum(a: i64, b: i64) -> i64 {
+    return a + b;
+}
+
+// mut func
+fn mut_num(a: &mut i64) {
+    *a = 5
+}
+
+// func ref
+
+fn ref_func(a: &i64) -> i64 {
+    *a + 1
+}
+
 fn main() {
     let mut answer = 0;
     println!("Hello {}", answer);
@@ -6,5 +23,14 @@ fn main() {
         answer += i;
     }
     println!("Hello {}", answer);
+
+    println!("sum of 4 and 5 is  {}", sum(4, 5));
+
+    let mut a = 2;
+    println!("first number of the a: {}", a);
+    mut_num(&mut a);
+    println!("the value of a before change: {}", a);
+    let new_value = ref_func(&a);
+    println!("the value of a before plus 1: {}", new_value);
     
 }
